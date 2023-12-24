be2c8cc5b48a303e8a9d04fef49cc179a21f319c
Abdouraouf <youssouf.abdouraouf4@gmail.com>
2023-12-24 10:42:24 +0100

    server

--- server/dist/src/domain/models/projets/projet.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProjetModel = void 0;
-class ProjetModel {
-}
-exports.ProjetModel = ProjetModel;
-//# sourceMappingURL=projet.model.js.map
--- server/dist/src/domain/models/users/verificationToken.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VerificationTokenType = void 0;
-var VerificationTokenType;
-(function (VerificationTokenType) {
-    VerificationTokenType["InitialVerification"] = "initial_verification";
-    VerificationTokenType["ResetEmail"] = "reset_email";
-})(VerificationTokenType || (exports.VerificationTokenType = VerificationTokenType = {}));
-//# sourceMappingURL=verificationToken.model.js.map
